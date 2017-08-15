9ec33ba3ca2fa635fd1e228345d819fb2e9dbef0
Anton Suprunov <suprunov.anton@gmail.com>
2017-08-15 19:02:46 +0300

    adjustments and png animation instead of gif

--- js/main.js
+++ js/main.js
@@ -1,23 +1,45 @@
 function main() {
-  var logo = document.querySelector('.logo__img');
+  var logo = document.querySelector('.logo__img'),
+    nav = document.querySelector('.nav'),
+    body = document.querySelector('body'),
+    isHome = false;
+
+  if (hasClass(body, 'home-page')) {
+    isHome = true;
+  }
+
+  if (isHome) {
+    var logos = document.querySelectorAll('.logo__img'),
+      i = 1;
+    console.log(logos);
+    setInterval(function() {
+      if (i === 0) {
+        removeClass(logos[logos.length - 1], 'logo__img_active');
+      } else {
+        removeClass(logos[i - 1], 'logo__img_active');
+      }
+      
+      addClass(logos[i], 'logo__img_active');
+      if (i === (logos.length - 1)) {
+        i = 0;
+      } else {
+        i++;
+      }
+    }, 600);
+  }
 
   setTimeout(function() {
-    //animate('.drops');
-    if (logo) {
-      logo.className = logo.className + ' bounce-in';
-    }
-
-    //setTimeout(function() {
     animate('.home-page .nav');
     setTimeout(function() {
-      //animate('.slogan');
-      //setTimeout(function() {
-      animate('.download');                
-      //}, 120);
+      animate('.download');
     }, 120);
-    //}, 120);
-
   }, 0);
+
+  nav.addEventListener('mouseover', function() {
+    if (nav.className.indexOf('nav_hovered') === -1) {
+      nav.className = nav.className + ' ' + 'nav_hovered';
+    } 
+  })
 
   // Set the size of the rendered Emojis
   // This can be set to 16x16, 36x36, or 72x72
@@ -32,6 +54,28 @@ function animate(selector) {
     var el = document.querySelector(selector);
     if (!el) return;
     el.className = el.className + ' animated_in';
+}
+
+function hasClass(el, className) {
+  if (el.classList)
+    return el.classList.contains(className)
+  else
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+}
+
+function addClass(el, className) {
+  if (el.classList)
+    el.classList.add(className)
+  else if (!hasClass(el, className)) el.className += " " + className
+}
+
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className)
+  else if (hasClass(el, className)) {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+    el.className=el.className.replace(reg, ' ')
+  }
 }
 
 document.addEventListener('DOMContentLoaded', main);
